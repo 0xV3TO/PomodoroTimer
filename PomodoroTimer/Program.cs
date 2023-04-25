5e3eb8bb23a8c844d03d3d5b0d1447bcde022fd9
0xV3TO <vitveto@proton.me>
2023-04-25 10:34:18 +0000

    Add project files.

---
+++ PomodoroTimer/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PomodoroTimer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Configure session duration: ");
+            int sessionMinutes = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Configure break duration: ");
+            int breakMinutes = Convert.ToInt32(Console.ReadLine());
+
+            Timer myTimer = new Timer(sessionMinutes, breakMinutes);
+        }
+    }
+}
---
+++ PomodoroTimer/Timer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomodoroTimer
+{
+    internal class Timer
+    {
+        public int sessionMinutes { get; set; }
+        public int breakMinutes { get; set; }
+        public Timer(int inputSessionMinutes, int inputBreakMinutes) { }
+    }
+}
