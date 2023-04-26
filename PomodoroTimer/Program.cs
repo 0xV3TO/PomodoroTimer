0d5760d4046a5498d93f03f143dfbe1f2c4aa44b
0xV3TO <131310147+0xV3TO@users.noreply.github.com>
2023-04-26 22:07:51 +0000

    Update Program.cs

--- PomodoroTimer/Program.cs
+++ PomodoroTimer/Program.cs
@@ -1,16 +1,34 @@
-﻿namespace PomodoroTimer
+namespace PomodoroTimer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Configure session duration: ");
-            int sessionMinutes = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.Write("Configure session duration: ");
+                int sessionMinutes = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Configure break duration: ");
-            int breakMinutes = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Configure break duration: ");
+                int breakMinutes = Convert.ToInt32(Console.ReadLine());
 
-            Timer myTimer = new Timer(sessionMinutes, breakMinutes);
+                Console.Write("Configure long break duration: ");
+                int longBreakMinutes = Convert.ToInt32(Console.ReadLine());
+
+                Timer myTimer = new Timer(sessionMinutes, breakMinutes, longBreakMinutes);
+
+                Console.Write("type 'start' to begin the counter: ");
+                string start = Console.ReadLine();
+                if (start == "start")
+                {
+                    myTimer.Start();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e);
+            }
+
         }
     }
 }
